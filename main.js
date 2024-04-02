a1b0a046cb7d40e79a424cd7f3b0e17e949e3232
alessianekielt <159924142+alessianekielt@users.noreply.github.com>
2024-04-02 11:16:31 -0300

    0204

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 const contadores = document.querySelectorAll(".contador");
+contadores[0].textContent = "Contagem regressiva";
 for (let i=0; i < botoes.length; i++){
     botoes [i].onclick = function() {
         for (let j=0; j<botoes.length; j++){
