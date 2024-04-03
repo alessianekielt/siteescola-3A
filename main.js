56d1f31c27228a1b4c5c5124cbed43d4d7b0d1d9
alessianekielt <159924142+alessianekielt@users.noreply.github.com>
2024-04-03 15:36:07 -0300

    0304

--- main.js
+++ main.js
@@ -12,6 +12,12 @@ for (let i=0; i < botoes.length; i++){
     }
 }
 const tempoObjetivo1 = new Date("2024-03-04T00:00:00");
+const tempoObjetivo2 = new Date("2024-12-07T00:00:00");
+const tempoObjetivo3 = new Date("2024-12-09T00:00:00");
+const tempoObjetivo4 = new Date("2024-12-011T00:00:00");
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+paramos aqui
+for (let i = 0; i < contadores.length; i++) {}
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
@@ -24,6 +30,6 @@ function calculaTempo(tempoObjetivo){
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
-    
+
     return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
 }
